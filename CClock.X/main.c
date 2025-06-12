a31e39368f912a10bbdc40f6f2235af575d38ee2
Mess <ian.marmendes@gmail.com>
2025-06-12 14:46:05 -0300

    finished

--- CClock.X/main.c
+++ CClock.X/main.c
@@ -11,6 +11,9 @@
 #include <stdio.h>
 #define _XTAL_FREQ 4000000
 
+#define ADDI PORTBbits.RB1
+#define SUBI PORTBbits.RB2
+
 #define T1H 0x0B
 #define T1L 0xDC 
 
@@ -41,116 +44,146 @@ char Seconds = 0;
 char state = 0;
 
 void main(void) {
-    char Line1[20];     //vari?vel para o fun??o sprintf
-    
-    char Line2[10];     //vari?vel para o fun??o sprintf
-  
-    TRISD = 0x00;  //configura PORTD como sa?da. Local onde deve estar o LCD
+    char Line1[20]; //vari?vel para o fun??o sprintf
+
+    char Line2[10]; //vari?vel para o fun??o sprintf
+
+    TRISD = 0x00; //configura PORTD como sa?da. Local onde deve estar o LCD
     TRISB = 0xff; // IO port b
-  
-    Lcd_Init();    //necess?rio para o LCD iniciar
-    
-    OPTION_REGbits.nRBPU    = 0; //portb PullUp
-    OPTION_REGbits.INTEDG   = 0; //interrupt on port0 rising edge 0 -> 1
-    OPTION_REGbits.T0CS     = 1; //TMR0 Clock Source Select bit 
-    OPTION_REGbits.T0SE     = 1; // TMR0 Source Edge Select bit
-    OPTION_REGbits.PSA      = 1; //Prescaler Assignment bit       1 = Prescaler is assigned to the WDT  0 = Prescaler is assigned to the Timer0 module  
-    OPTION_REGbits.PS0      = 0; // Prescaler Rate Select bits
-    OPTION_REGbits.PS1      = 0;
-    OPTION_REGbits.PS2      = 0;
-                    
-    INTCONbits.GIE    = 1;  // global interrupt enable     
-    INTCONbits.PEIE   = 1;  // Peripheral Interrupt Enable bit   
-    INTCONbits.TMR0IE = 0;  // TMR0 Overflow Interrupt Enable bit     
-    INTCONbits.INTE   = 1;  // RB0/INT External Interrupt Enable bit
-    INTCONbits.RBIE   = 0;  // RB Port Change Interrupt Enable bit     
-    INTCONbits.TMR0IF = 0;  // TMR0 Overflow Interrupt Flag bit    (must be cleared in software)
-    
-    
-    INTCONbits.INTF   = 0;  // RB0/INT External Interrupt Flag bit (must be cleared in software)
-    INTCONbits.RBIF   = 0;  // RB Port Change Interrupt Flag bit   (must be cleared in software)
-    PIE1bits.TMR1IE = 1;    //Habilita int do timer 1
-    
+
+    Lcd_Init(); //necess?rio para o LCD iniciar
+
+    OPTION_REGbits.nRBPU = 0; //portb PullUp
+    OPTION_REGbits.INTEDG = 0; //interrupt on port0 rising edge 0 -> 1
+    OPTION_REGbits.T0CS = 1; //TMR0 Clock Source Select bit 
+    OPTION_REGbits.T0SE = 1; // TMR0 Source Edge Select bit
+    OPTION_REGbits.PSA = 1; //Prescaler Assignment bit       1 = Prescaler is assigned to the WDT  0 = Prescaler is assigned to the Timer0 module  
+    OPTION_REGbits.PS0 = 0; // Prescaler Rate Select bits
+    OPTION_REGbits.PS1 = 0;
+    OPTION_REGbits.PS2 = 0;
+
+    INTCONbits.GIE = 1; // global interrupt enable     
+    INTCONbits.PEIE = 1; // Peripheral Interrupt Enable bit   
+    INTCONbits.TMR0IE = 0; // TMR0 Overflow Interrupt Enable bit     
+    INTCONbits.INTE = 1; // RB0/INT External Interrupt Enable bit
+    INTCONbits.RBIE = 0; // RB Port Change Interrupt Enable bit     
+    INTCONbits.TMR0IF = 0; // TMR0 Overflow Interrupt Flag bit    (must be cleared in software)
+
+
+    INTCONbits.INTF = 0; // RB0/INT External Interrupt Flag bit (must be cleared in software)
+    INTCONbits.RBIF = 0; // RB Port Change Interrupt Flag bit   (must be cleared in software)
+    PIE1bits.TMR1IE = 1; //Habilita int do timer 1
+
     T1CONbits.TMR1CS = 0;
-    T1CONbits.T1CKPS0 = 1;  //bit pra configurar pre-escaler, nesta caso 1:8
-    T1CONbits.T1CKPS1 = 1; 
-    
+    T1CONbits.T1CKPS0 = 1; //bit pra configurar pre-escaler, nesta caso 1:8
+    T1CONbits.T1CKPS1 = 1;
+
     //1khz / 8
-    
-    
-    
-    TMR1L = T1L;          //carga do valor inicial no contador (65536-62500)
-    TMR1H = T1H;          //3036. Quando estourar contou 62500, passou 0,5s   
-   
-    
+
+
+
+    TMR1L = T1L; //carga do valor inicial no contador (65536-62500)
+    TMR1H = T1H; //3036. Quando estourar contou 62500, passou 0,5s   
+
+
     //todo pedir p professor re-explicar
-    
-    T1CONbits.TMR1ON = 1;   //Liga o timer
-    
-    
-    
-   
-    
-    while(1){
-       Lcd_Clear(); //limpa LCD
-       if(state == 0) T1CONbits.TMR1ON = 1; 
-        switch(state){
-            
-            case 0:  
-                sprintf(Line1, "%i:%i:%i", (int)Hours,(int)Minutes,(int)Seconds);  //Armazena em buffer o conte?do da vari?vel f formatado com duas casas
-                Lcd_Set_Cursor(1,1);                  
-                Lcd_Write_String(Line1);             //Escreve o conte?do de buffer no LCD
-                
+
+    T1CONbits.TMR1ON = 1; //Liga o timer
+
+
+
+
+
+    while (1) {
+        Lcd_Clear(); //limpa LCD
+        if (state == 0) T1CONbits.TMR1ON = 1;
+        switch (state) {
+
+            case 0:
+                sprintf(Line1, "%i:%i:%i", (int) Hours, (int) Minutes, (int) Seconds); //Armazena em buffer o conte?do da vari?vel f formatado com duas casas
+                Lcd_Set_Cursor(1, 1);
+                Lcd_Write_String(Line1); //Escreve o conte?do de buffer no LCD
+                __delay_ms(500);
                 break;
             case 1:
-                T1CONbits.TMR1ON = 0; 
-                sprintf(Line1, "%i:%i:%i", (int)Hours,(int)Minutes,(int)Seconds);  //Armazena em buffer o conte?do da vari?vel f formatado com duas casas
-                Lcd_Set_Cursor(1,1);                  
-                Lcd_Write_String(Line1);             //Escreve o conte?do de buffer no LCD
-                Lcd_Set_Cursor(2,1);                  
-                Lcd_Write_String("Editing");    
+                T1CONbits.TMR1ON = 0;
+                sprintf(Line1, "%i:%i:%i", (int) Hours, (int) Minutes, (int) Seconds); //Armazena em buffer o conte?do da vari?vel f formatado com duas casas
+                Lcd_Set_Cursor(1, 1);
+                Lcd_Write_String(Line1); //Escreve o conte?do de buffer no LCD
+                Lcd_Set_Cursor(2, 1);
+                Lcd_Write_String("Editing Minutes");
+                if (ADDI == 0) {
+                    Minutes++;
+                }
+                if (SUBI == 0) {
+                    Minutes--;
+                }
                 
-                
+
+                __delay_ms(150);
+
                 break;
             case 2:
+                T1CONbits.TMR1ON = 0;
+                sprintf(Line1, "%i:%i:%i", (int) Hours, (int) Minutes, (int) Seconds); //Armazena em buffer o conte?do da vari?vel f formatado com duas casas
+                Lcd_Set_Cursor(1, 1);
+                Lcd_Write_String(Line1); //Escreve o conte?do de buffer no LCD
+                Lcd_Set_Cursor(2, 1);
+                Lcd_Write_String("Editing Hours");
+                if (ADDI == 0) {
+                    Hours++;
+                }
+                if (SUBI == 0) {
+                    Hours--;
+                }
+                
+                __delay_ms(150);
                 break;
         }
-        __delay_ms(100);
+        if (Minutes > 59) {
+            Hours++;
+            Minutes = 0;
+        }
+        if (Hours > 23) {
+            Hours = 0;
+        }
+        
     }
-    
+
     return;
 }
 
 void __interrupt() TrataInt(void)
-
-{    
-    if(INTF == 1){
+ {
+    if (INTF == 1) {
         INTCONbits.INTF = 0;
         state++;
-                if (state >2){state = 0;}
+        if (state > 2) {
+            state = 0;
+        }
     }
-    if (TMR1IF)  //foi a interrup??o de estouro do timer1 que chamou a int?
-     {  
+    if (TMR1IF) //foi a interrup??o de estouro do timer1 que chamou a int?
+    {
         PIR1bits.TMR1IF = 0; //reseta o flag da interrup??o
-        TMR1L = T1L;          //carga do valor inicial no contador (65536-62500)
-        TMR1H = T1H;          //3036. Quando estourar contou 62500, passou 0,5s   
-     
-        
+        TMR1L = T1L; //carga do valor inicial no contador (65536-62500)
+        TMR1H = T1H; //3036. Quando estourar contou 62500, passou 0,5s   
+
+
         Seconds++;
-        if(Seconds > 59){
+        if (Seconds > 59) {
             Minutes++;
             Seconds = 0;
         }
-        if(Minutes > 59){
+        if (Minutes > 59) {
             Hours++;
             Minutes = 0;
         }
-        if(Hours > 23){
+        if (Hours > 23) {
             Hours = 0;
         }
-        
+
         //comandos pra tratar a interrup??o
-        
-  }
-  return;
+
+    }
+    return;
 }
